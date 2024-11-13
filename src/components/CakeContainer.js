eb9e74c974aa7f45e268649d86f19d72cba5a035
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-11-13 13:19:26 +0530

    dispatching actions

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { cakeActions } from '../redux/features/cake/cakeSlice'
 
 const CakeContainer = () => {
     const numOfCake=useSelector((state)=>state.cake.numOfCakes)
+    const dispatch=useDispatch()
     console.log(numOfCake)
   return (
     <div>
     <h2>No of Cakes :- {numOfCake} </h2>
-    <button>Buy Cake</button>
+    <button onClick={()=>dispatch(cakeActions.ordered())}>Buy Cake</button>
+    <button onClick={()=>dispatch(cakeActions.restocked(5))}>Restock</button>
 </div>
   )
 }
